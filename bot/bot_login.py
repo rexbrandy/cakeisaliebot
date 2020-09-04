72cef7ebf977c0c2c1c6c5dc19112511c05e7417
bailey <bailey.sdev@gmail.com>
2020-09-04 13:44:43 +0200

    Changes to bot login to satisfy heroku

--- bot/bot_login.py
+++ bot/bot_login.py
@@ -1,6 +1,13 @@
 import praw
+import os
 
 def login():
-    r = praw.Reddit('CAKE', user_agent='The cake is a lie bot v1')
+    r = praw.Reddit(
+        username = os.environ["reddit_username"],
+        password = os.environ["reddit_password"],
+        client_id = os.environ["client_id"],
+        client_secret = os.environ["client_secret"],
+        user_agent='The cake is a lie bot v1, Github: https://github.com/rexbrandy/cakeisaliebot'
+    )
 
     return r
